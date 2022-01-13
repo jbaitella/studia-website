5ae56a7880cf45a5b818dd1e73220dba4ebe3b93
jbaitella <89507775+jbaitella@users.noreply.github.com>
2022-01-13 12:25:25 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function() {
+     if (document.getElementById("button")) 
+{
+ document.getElementById("button")
+ .addEventListener("click", test);
+ }
+});
+
+function test() {
+     alert ('you pisser!');
+}
+
