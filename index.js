ffb77debe1a0ccf71bba86ced771cb4e96eeba67
jbaitella <89507775+jbaitella@users.noreply.github.com>
2022-01-16 14:03:36 +0100

    D

---
+++ index.js
@@ -0,0 +1,28 @@
+let DARKMODE = false;
+const BUTTON= document.getElementById('switch');
+const  BODY= document.querySelector('Body');
+
+if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+    DARKMODE=true;
+    }else{
+        DARKMODE = false;
+
+}
+const switchMode= () => {
+    DARKMODE= !DARKMODE;
+    setClassToBody();
+
+};
+
+const setClassToBody = () => {
+    if (DARKMODE) {
+        BODY.classList.remove('light');
+        BODY.classList.add('dark');
+    }else{
+        BODY.classList.add('light');
+        BODY.classList.remove('dark');
+
+    }
+};
+BUTTON.addEventListener('click', switchMode);
+setClassToBody();
